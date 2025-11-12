37642f2e39ec867f82fc42031b4486f01911674b
Lyric <vertiipavel0684@gmail.com>
2025-11-12 15:16:03 +0300

    Service, DI: - Описал сервис для менеджмента отложенных запросов. - Описал зависимость для сервиса отложенных запросов

---
+++ backend/src/fastapi_voting/app/services/task_service.py
@@ -0,0 +1,41 @@
+import json
+
+from datetime import timedelta, timezone
+
+from src.fastapi_voting.app.core.settings import get_settings
+
+from redis.asyncio import Redis
+
+
+# --- Инструментарий ---
+settings = get_settings()
+
+
+# --- Сервис ---
+class TaskService:
+    """Выполнять роль менеджера для фоновых задач"""
+
+    def __init__(self, redis: Redis):
+        self.redis = redis
+
+    async def add_change_password_task(self, user_id: int, new_password: str):
+        """Создаёт отложенный запроса на смену пароля"""
+
+        # --- Запись в Redis ---
+        await self.redis.setex(
+            name=f"pending-password:{user_id}",
+            time=timedelta(hours=settings.EMAIL_SUBMIT_EXPIRE_HOURS),
+            value=new_password
+        )
+
+
+    async def execute_change_password_task(self, user_id: int):
+        """Извлекает данные отложенного запроса на смену пароля и удаляет запись"""
+
+        # --- Чтение пароля и удаление записи ---
+        password = await self.redis.getex(
+            name=f"pending-password:{user_id}",
+            px=1
+        )
+        # --- Ответ ---
+        return password.decode("utf-8")
