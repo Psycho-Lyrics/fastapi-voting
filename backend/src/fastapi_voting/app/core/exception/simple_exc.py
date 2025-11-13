dbc0b8ffa1af20c1b548005df4b9de0362e50a4a
Lyric <vertiipavel0684@gmail.com>
2025-11-13 08:43:38 +0300

    Рефакторинг, Exception: Вынес простые исключения в отдельный модуль

---
+++ backend/src/fastapi_voting/app/core/exception/simple_exc.py
@@ -0,0 +1,22 @@
+from fastapi import status
+
+from src.fastapi_voting.app.core.exception.base_exc import AppException
+
+
+# --- Исключения для пользователей ---
+class UserNotFound(AppException):
+    def __init__(self, log_message: str):
+        super().__init__(log_detail=log_message, detail=f"Invalid Data.", status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class UserAlreadyExist(AppException):
+    def __init__(self, log_message: str):
+        super().__init__(log_detail=log_message, detail=f"Invalid Data.", status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class InvalidLogin(AppException):
+    def __init__(self, log_message: str):
+        super().__init__(log_detail=log_message, detail=f"Invalid Data.", status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+# --- Исключения для голосований ---
+class VotingNotFound(AppException):
+    def __init__(self, log_message: str):
+        super().__init__(log_detail=log_message, detail=f"Invalid Data.", status_code=status.HTTP_404_NOT_FOUND)
