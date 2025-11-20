34cc06a0bd3a58bb0536dc8938b8beb563f99b1a
Lyric <vertiipavel0684@gmail.com>
2025-11-20 13:19:16 +0300

    Exception: Классы исключений для отложенных задач

--- backend/src/fastapi_voting/app/core/exception/simple_exc.py
+++ backend/src/fastapi_voting/app/core/exception/simple_exc.py
@@ -16,7 +16,20 @@ class InvalidLogin(AppException):
     def __init__(self, log_message: str):
         super().__init__(log_detail=log_message, detail=f"Invalid Data.", status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
+
 # --- Исключения для голосований ---
 class VotingNotFound(AppException):
     def __init__(self, log_message: str):
         super().__init__(log_detail=log_message, detail=f"Invalid Data.", status_code=status.HTTP_404_NOT_FOUND)
+
+
+# --- Исключения для отложенных тасков ---
+class TaskAlreadyExist(AppException):
+    def __init__(self, log_message: str):
+        super().__init__(log_detail=log_message, detail="Invalid Data", status_code=status.HTTP_409_CONFLICT)
+
+class TaskNotFound(AppException):
+    def __init__(self, log_message: str):
+        super().__init__(log_detail=log_message, detail="Invalid Data", status_code=status.HTTP_404_NOT_FOUND)
+
+
