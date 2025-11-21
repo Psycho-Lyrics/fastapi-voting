7719ed223871920cf84e91b1c6a3784fcb0eeb40
Lyric <vertiipavel0684@gmail.com>
2025-11-21 09:41:38 +0300

    API: Делегировал обработчики для работы с пользователями

---
+++ backend/src/fastapi_voting/app/api/user/user_profile_api.py
@@ -0,0 +1,83 @@
+from uuid import UUID
+
+from fastapi import APIRouter, status, Header
+
+from src.fastapi_voting.app.core.settings import get_settings
+
+from src.fastapi_voting.app.di.annotations import (
+    UserServiceAnnotation,
+
+    AccessRequiredAnnotation,
+)
+from src.fastapi_voting.app.schemas.user_schema import (
+    UserSchema,
+    InputChangeCredentialsSchema,
+    InputChangePasswordSchema
+)
+
+# --- Инструментарий и обработчик ---
+settings = get_settings()
+
+user_profile_router = APIRouter(
+    prefix="/profile",
+    tags=["Личный кабинет пользователя"]
+)
+
+
+# -- Смена учётных данных ---
+@user_profile_router.post("/change-credentials", response_model=UserSchema, status_code=status.HTTP_200_OK)
+async def change_user_credentials(
+        user_service: UserServiceAnnotation,
+        access_payload: AccessRequiredAnnotation,
+
+        data: InputChangeCredentialsSchema,
+
+        access_token = Header(default=None, description="JWT-Токен"),
+):
+    # Работа со входными данными запроса
+    data = data.model_dump()
+    user_id = access_payload["sub"]
+
+    # Работа сервиса
+    user = await user_service.change_credentials(data, user_id)
+
+    # Ответ
+    return user
+
+
+# -- Смена пароля ---
+@user_profile_router.post("/change-password", status_code=status.HTTP_200_OK)
+async def change_user_password_init(
+        access_payload: AccessRequiredAnnotation,
+        data: InputChangePasswordSchema,
+
+        user_service: UserServiceAnnotation,
+
+        access_token = Header(default=None, description="JWT-Токен"),
+):
+    # Первичные данные
+    data = data.model_dump()
+    user_id = access_payload["sub"]
+
+    # Работа сервиса
+    await user_service.init_change_password(data, user_id)
+
+    return {"message": "email message sent"}
+
+
+@user_profile_router.post("/change-password-confirm/{uuid}", status_code=status.HTTP_200_OK)
+async def change_user_password_confirm(
+        access_token_payload: AccessRequiredAnnotation,
+        user_service: UserServiceAnnotation,
+
+        uuid: UUID,
+        token=Header(default=None, description="JWT-токен"),
+):
+    # Данные запроса
+    user_id = access_token_payload["sub"]
+
+    # Работа сервиса
+    await user_service.confirm_change_password(user_id=user_id, uuid=uuid)
+
+    # Ответ
+    return {"message": "password changed"}
--- backend/src/fastapi_voting/app/main/main.py
+++ backend/src/fastapi_voting/app/main/main.py
@@ -4,7 +4,9 @@ from logging import getLogger
 from src.fastapi_voting.app.main.middlewares import setup_middlewares
 from src.fastapi_voting.app.main.handlers import setup_handlers
 
-from src.fastapi_voting.app.api.user_api import user_router
+from src.fastapi_voting.app.api.user.user_auth_api import user_auth_router
+from src.fastapi_voting.app.api.user.user_profile_api import user_profile_router
+
 from src.fastapi_voting.app.api.department_api import department_router
 from src.fastapi_voting.app.api.voting_api import voting_router
 
@@ -29,6 +31,8 @@ setup_handlers(fastapi_app)
 v1_url_prefix = '/api/v1'
 
 # --- Регистрация обработчиков маршрутов ---
-fastapi_app.include_router(router=user_router, prefix=v1_url_prefix)
+fastapi_app.include_router(router=user_auth_router, prefix=v1_url_prefix)
+fastapi_app.include_router(router=user_profile_router, prefix=v1_url_prefix)
+
 fastapi_app.include_router(router=department_router, prefix=v1_url_prefix)
 fastapi_app.include_router(router=voting_router, prefix=v1_url_prefix)
